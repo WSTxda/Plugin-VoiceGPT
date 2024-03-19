acd51f5ac50e2cc563a6a57945ef395633095935
WSTxda <wstxda@gmail.com>
2024-03-19 02:37:08 -0300

    Update Gradle and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.wstxda.voicegpt"
         minSdk = 24
         targetSdk = 34
-        versionCode = 110
-        versionName = "1.1"
+        versionCode = 120
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
